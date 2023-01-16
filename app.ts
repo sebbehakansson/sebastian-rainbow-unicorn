9659fa330aab4ece91c44c5f277473069f91e8da
sebbehakansson <sebastian.sh74@gmail.comm>
2023-01-16 13:06:59 +0100

    first commit

---
+++ app.ts
@@ -0,0 +1,16 @@
+"övning ts"
+
+const animals: string[] = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
+console.log(animals);
+
+type Testperson = {
+    name: string;
+    age: number;
+    partner: boolean;
+}
+
+const person: Testperson[] = [
+    name: "hasse",
+    age: 12,
+    partner: true,
+];
